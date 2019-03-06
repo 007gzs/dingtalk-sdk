453f9400f0491ec5ceb1b7c255929acf568fb889
007gzs <007gzs@gmail.com>
2019-03-06 16:32:26 +0800

    Bump version: 1.1.9 → 1.2.0

--- dingtalk/__init__.py
+++ dingtalk/__init__.py
@@ -6,7 +6,7 @@ from dingtalk.client import SecretClient, AppKeyClient  # NOQA
 from dingtalk.client.isv import ISVClient  # NOQA
 from dingtalk.core.exceptions import DingTalkClientException, DingTalkException  # NOQA
 
-__version__ = '1.1.9'
+__version__ = '1.2.0'
 __author__ = '007gzs'
 
 # Set default logging handler to avoid "No handler found" warnings.
