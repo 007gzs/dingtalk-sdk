f12d9bde3a6446ea299d59ced0cd65fed872c898
007gzs <007gzs@gmail.com>
2019-07-03 17:43:24 +0800

    pep8

--- dingtalk/client/api/extcontact.py
+++ dingtalk/client/api/extcontact.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-import six
-
 from dingtalk.client.api.base import DingTalkBaseAPI
-from dingtalk.core.utils import json_loads
 
 
 class ExtContact(DingTalkBaseAPI):
