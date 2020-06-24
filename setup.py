d7734a21c0e36c7c534e576bf1b30fa95cb2f046
007gzs <007gzs@gmail.com>
2020-06-24 17:18:09 +0800

    pep8

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 with open('requirements.txt') as f:
-    requirements = [l for l in f.read().splitlines() if l]
+    requirements = [line for line in f.read().splitlines() if line]
 
 setup(
     name='dingtalk-sdk',
